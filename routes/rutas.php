eef1ab98acbbd20450a2877d470949497f7b5d93
Yessica <yessicamelendez0708@gmail.com>
2026-06-06 22:11:54 -0400

    Creando devoluciones

--- routes/rutas.php
+++ routes/rutas.php
@@ -59,6 +59,7 @@ function manejarRuta($pagina): void
         'Premios' => 'Premios',
         'Reportes' => 'Reportes',
         'Catalogos'=>'Catalogos',
+        'Devoluciones' => 'Devoluciones',
     ];
     // Verificamos si la página solicitada existe en las rutas definidas
     if (array_key_exists($pagina, $rutas)) {
